c3eccd53cbaff7d0ed2bcf197d0d6ef162f35c44
MatyAB13 <130349949+MatyAB13@users.noreply.github.com>
2026-02-26 22:30:17 -0300

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-storage.js";
 
 // ✅ Pega aquí TUS datos de Firebase
 const firebaseConfig = {
@@ -16,5 +17,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+const storage = getStorage(app);
 
-export { auth, db };
+export { auth, db, storage };
+
